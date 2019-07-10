4806e394afcc435e62b9fbb22d8d49808642f70c
Anders Stromberg <coltlaboy@yahoo.se>
2019-07-10 10:07:45 +0200

    Divide Function

--- script.py
+++ script.py
@@ -1,6 +1,18 @@
-import requests
+def add(x, y):
+    pass
 
-r = requests.get("https://coreyms.com")
-print(r.status_code)
-print(r.ok)
-print('Done!')
+
+def subtract(x, y):
+    pass
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+def square(x):
+    pass
